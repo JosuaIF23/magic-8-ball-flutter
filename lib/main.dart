d0576a1b7016e606cdb90d8651dc3089911b00be
JosuaIF23 <142556166+JosuaIF23@users.noreply.github.com>
2025-12-27 11:47:54 +0700

    Add Backgroud and Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: null));
+void main() => runApp(MaterialApp(home: BallPage()));
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Ask Me Anything',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Container(),
+    );
+  }
+}
